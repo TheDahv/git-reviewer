2939e81f70d33a60327ce125f8f7e661ab9bc287
David Pierce <david.pierce@moz.com>
2015-08-22 15:33:37 -0700

    Adds example test for getting commit counts

--- src/gitsh_test.go
+++ src/gitsh_test.go
@@ -1,6 +1,9 @@
 package gitreviewers
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestChangedFiles(t *testing.T) {
 	lines, err := changedFiles()
@@ -11,5 +14,26 @@ func TestChangedFiles(t *testing.T) {
 
 	if len(lines) == 0 {
 		t.Error("Got 0 lines, expected more")
+	}
+}
+
+func TestCommitterCounts(t *testing.T) {
+	path := os.Getenv("GOPATH") +
+		"/src/github.com/thedahv/git-reviewer/src"
+
+	counts, err := committerCounts(path)
+
+	if err != nil {
+		t.Errorf("Got error %v, expected none\n", err)
+		t.FailNow()
+	}
+
+	if len(counts) == 0 {
+		t.Errorf("Got 0 counts, expected more")
+		t.FailNow()
+	}
+
+	if counts[0].Reviewer == "" || counts[0].Count == 0 {
+		t.Errorf("Got empty stats where we didn't expect to")
 	}
 }
